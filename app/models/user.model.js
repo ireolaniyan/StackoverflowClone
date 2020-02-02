b336cc0dd0fc58e643c9208d56990f42a99c9bb2
Ire Olaniyan <ireoluwa@crenettechlabs.com>
2020-02-02 22:12:42 +0100

    chore: create User model

---
+++ app/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+	first_name: String,
+	last_name: String,
+	password: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	}
+})
+
+UserSchema.pre('save', async function (next) {
+	const user = this
+	const hash = await bcrypt.hash(user.password, 10)
+	user.password = hash
+	next()
+})
+
+UserSchema.methods.isValidPassword = async function (password) {
+	const user = this
+	const comparePasswords = await bcrypt.compare(password, user.password)
+	return comparePasswords
+}
+
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = UserModel
