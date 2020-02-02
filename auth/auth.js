f52f48ef190cc0ec951d11c4275140e428897b08
Ire Olaniyan <ireoluwa@crenettechlabs.com>
2020-02-02 22:38:28 +0100

    chore: add middleware for signin

--- auth/auth.js
+++ auth/auth.js
@@ -18,3 +18,29 @@ passport.use('signup', new localStrategy({
 	}
 }))
 
+// Passport middleware to handle User signin
+passport.use('login', new localStrategy({
+	usernameField: 'email',
+	passwordField: 'password'
+}, async (email, password, done) => {
+	try {
+		// Find the user associated with the email
+		const user = await UserModel.findOne({ email })
+
+		if (!user) {
+			return done(null, false, { message: 'User not found' })
+		}
+
+		// Validate password and make sure it matches with the corresponding hash stored in the database
+		const validateUser = await user.isValidPassword(password)
+		if (!validateUser) {
+			return done(null, false, { message: 'Incorrect Password' })
+		}
+
+		//Send the user information to the next middleware
+		return done(null, user, { message: 'Logged in Successfully' })
+
+	} catch (error) {
+		return done(error)
+	}
+}))
