1a7acc5fab02ff1690173c7bbb2767d2dca5a72b
Ire Olaniyan <ireoluwa@crenettechlabs.com>
2020-02-02 21:36:06 +0100

    feat: configure database

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const database = require('./config/database.config.js')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(database.url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+	console.log("Successfully connected to the database");
+}).catch(err => {
+	console.log('Could not connect to the database. Exiting now...', err)
+	process.exit()
+})
+
+
 app.get('/', (req, res) => {
 	res.json({ "message": "Welcome to Stack Overflow" })
 })
