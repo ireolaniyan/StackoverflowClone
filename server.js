39cd31bff34c4a45fa57ec63c588fb816503a026
Ire Olaniyan <ireolaniyan7@gmail.com>
2020-05-18 15:11:57 +0100

    chore: set database name in database config file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 mongoose.connect(database.url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-	console.log("Successfully connected to the database");
+	console.log("Successfully connected to the database 😃");
 }).catch(err => {
 	console.log('Could not connect to the database. Exiting now...', err)
 	process.exit()
