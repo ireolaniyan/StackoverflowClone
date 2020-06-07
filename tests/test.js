03e9a5ec134e8c1249c69ccfd999500d440c7556
Ire Olaniyan <ireolaniyan7@gmail.com>
2020-06-07 14:46:40 +0100

    feat: write test for successful user creation

---
+++ tests/test.js
@@ -0,0 +1,34 @@
+const assert = require("assert");
+const chaiHttp = require("chai-http");
+const chai = require("chai");
+const { expect } = require('chai');
+
+const app = require("../server");
+const { getDb } = require("../config/database.config")
+
+chai.use(chaiHttp);
+
+const userDetails = {
+  first_name: "Adeola",
+  last_name: "Olaniyan",
+  email: "adeola@gmail.com",
+  password: "111111q"
+}
+
+describe("Users", () => {
+  after(() => {
+    const db = getDb()
+    db.connection.db.dropDatabase()
+  })
+
+  describe("Create User", () => {
+    it("Should add a new user to the db", async () => {
+      const res = await chai.request(app).post("/signup").send(userDetails)
+      expect(res.status).to.eqls(201);
+      expect(res.body.success).to.eqls(true);
+      expect(res.body.data).to.be.an('object');
+      expect(res.body.message).to.eqls("Successfully Created User")
+    })
+  })
+
+})
