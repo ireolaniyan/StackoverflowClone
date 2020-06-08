386e7fe94f09a28831516293be785e2727bd1c5f
Ire Olaniyan <ireolaniyan7@gmail.com>
2020-06-08 01:13:34 +0100

    chore: use joi to validate registration fields

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,10 +1,35 @@
 const User = require('../models/user.model.js')
 
+const Joi = require('joi')
+
 exports.create = async (req, res) => {
 	// Create a new user
 	try {
-		const user = new User(req.body)
-		await user.save()
+		const { first_name, last_name, email, password } = req.body
+
+		const schema = Joi.object().keys({
+			first_name: Joi.string().required(),
+			last_name: Joi.string().required(),
+			email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+			password: Joi.string().alphanum().min(6).max(16).required()
+		})
+
+		const validation = Joi.validate(req.body, schema)
+		if (validation.error !== null) {
+			return res.status(400).send({
+				success: false,
+				message: validation.error.details[0].message
+			})
+		}
+
+		const duplicateMail = await User.findOne({ email })
+		if (duplicateMail)
+			return res.status(400).send({
+				success: false,
+				message: "Email Already Exists"
+			})
+
+		const user = await User.create({ first_name, last_name, email, password })
 		await user.generateAuthToken()
 
 		res.status(201).send({
